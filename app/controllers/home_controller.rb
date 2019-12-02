16aa54f6814beaf03d3f1ed10d1e548aab072041
Michael Dao <mike@turing.io>
2019-12-02 09:26:36 -0700

    changes date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ require "date"
 
 class HomeController < ApplicationController
   def index
-    start_date = Date.new(2019, 12, 02)
+    start_date = Date.new(2019, 12, 01)
     @days_since = (Date.today - start_date).to_i
   end
 end
